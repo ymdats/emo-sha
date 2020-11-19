89fb678fe7b3385520e81e1fef5a46b34fbe95d4
ymdats <61939802+ymdats@users.noreply.github.com>
2020-11-19 18:34:05 +0900

    詳細ページ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def show
-    # @item = Item.find(1)
+    @item = Item.find(1)
     # @evaluation = Evaluation.new
     # @evaluations = @item.evaluations.includes(:user)
   end
