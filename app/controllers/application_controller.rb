39492048f3d55731865506429e1fcad2a7e8624c
ymdats <61939802+ymdats@users.noreply.github.com>
2020-11-21 17:49:10 +0900

    ログイン/アウト遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,12 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #   redirect_to action: :show
-  #   request.referer
-  # end
-  # def after_sign_out_path_for(resource)
-    # redirect_to action: :show
-    # request.referer
-  # end
+  def after_sign_in_path_for(resource)
+    request.referer
+  end
+  def after_sign_out_path_for(resource)
+    request.referer
+  end
 
   protected
   def configure_permitted_parameters
