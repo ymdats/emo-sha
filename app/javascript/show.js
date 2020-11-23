bb27c317a9025a1518201d0c303ea1bde4d96405
ymdats <61939802+ymdats@users.noreply.github.com>
2020-11-23 19:34:51 +0900

    選択/入力しないとボタン押せない

--- app/javascript/show.js
+++ app/javascript/show.js
@@ -1,12 +1,30 @@
+document.addEventListener("turbolinks:load"
+, function () {
+
 $(function() {
-  if ( $('.form-comment').val().length == 0 ) {
-    $('.comment-submit').attr('disabled', 'disabled');
+  if ( $('.form-comment').val() === "" ) {
+    $('.submit').attr('disabled', 'disabled');
   }
   $('.form-comment').bind('keydown keyup keypress change', function() {
-    if ( $(this).val().length > 0 ) {
-      $('.comment-submit').removeAttr('disabled');
+    if ( $(this).val() !== "" ) {
+      $('.submit').removeAttr('disabled');
     } else {
-      $('.comment-submit').attr('disabled', 'disabled');
+      $('.submit').attr('disabled', 'disabled');
     }
   });
+});
+
+$(function() {
+  if ( $('.select').val() === "" ) {
+    $('.submit').attr('disabled', 'disabled');
+  }
+  $('.select').bind('keydown keyup keypress change', function() {
+    if ( $(this).val() !== "" ) {
+      $('.submit').removeAttr('disabled');
+    } else {
+      $('.submit').attr('disabled', 'disabled');
+    }
+  });
+});
+
 });
