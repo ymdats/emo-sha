96aba6163a8de479d9970454026f84173b0d5edc
ymdats <61939802+ymdats@users.noreply.github.com>
2020-11-23 19:33:20 +0900

    評価ボタン後のアクション指定

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,7 +1,20 @@
 class EvaluationsController < ApplicationController
+
   def create
+    
     evaluation = Evaluation.create(evaluation_params)
-    redirect_to "/items/#{evaluation.item.id}"
+
+    if $item_menu == []
+    # 配列が空になったらparams[:id]にfinishを代入
+    redirect_to "/items/finish"
+    else
+    # 配列の中からランダムにitemモデルのid取得+削除
+    random = rand($item_menu.length)
+    $item_choice = $item_menu[random]
+    $item_menu.delete_at(random)
+    redirect_to "/items/#{$item_choice}"
+    end
+
   end
 
   private
